e761e8b7295e1e88cdcd10b26ee70bcaaeaa5ea8
Anushka <Anushka@123>
2023-04-19 18:47:12 +0530

    first commit

---
+++ src/main/java/mypkg3/MyCalc.java
@@ -0,0 +1,16 @@
+package mypkg3;
+
+public class MyCalc {
+	public int sum(int a ,int b)
+	{
+		return(a+b);
+		
+	}
+	public int mul(int a ,int b)
+	{
+		return(a*b);
+	}
+	
+}
+
+
---
+++ src/test/java/devops/b4/lab2/MyCalcTest.java
@@ -0,0 +1,22 @@
+package devops.b4.lab2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import mypkg3.MyCalc;
+
+public class MyCalcTest {
+
+	@Test
+	public void test() {
+		MyCalc ob = new MyCalc();
+		
+ 
+         assertEquals(25, ob.sum(20, 5));
+         assertEquals(100, ob.mul(20, 5));
+         
+	}
+   
+    
+}
